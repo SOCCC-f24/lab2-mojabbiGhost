f80d786bac9f14922f392f0e48bacda88ef8cce6
mojabbiGhost <ak132@sunyorange.edu>
2024-10-13 15:49:53 -0400

    Update c_cipher.py17

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -12,12 +12,10 @@ def encrypt(email="abc012"):
         TODO: what varibale and data types are being returned?   
     """
     output = "" 
-    len_flag = len(email) != 6
-    anum_flag = email[:3] != 'abc' or email[3:] != '012' 
 
-    if len_flag:
+    if len_flag len_flag = len(email) != 6
         print("Email must be 6 characters long.")        
-    elif anum_flag:
+    elif anum_flag = email[:3] != 'abc' or email[3:] != '012'
         print("Email must have 3 letters followed by 3 digits.")
         
     # TODO: fix line below, process our string into a list
@@ -42,18 +40,16 @@ def decrypt(email="def345"):
     
     # input validation
     output = "" 
-    len_flag = len(email) != 6
     # TODO: fix line below and, implement functionality rather than literals
     # keep all updates in the anum_flag (bool) variable
     # i.e., 
     #     A = email[:3] (check first half)
     #     B = email[3:] (check second half)
     #     enum_flag = A or B
-    anum_flag = email[:3] != 'def' or email[3:] != '345' 
 
-    if len_flag:
+    if len_flag = len(email) != 6
         print("Email must be 6 characters long.")     
-    elif anum_flag:
+    elif anum_flag = email[:3] != 'def' or email[3:] != '345'
         print("Email must have 3 letters followed by 3 digits.") 
 
     # TODO: apply the encrypt pseudocode but shift down 3
