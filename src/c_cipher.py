32f8a92d2200a8b74ee81956e2190842971a6603
mojabbiGhost <ak132@sunyorange.edu>
2024-10-14 22:45:30 -0400

    Update c_cipher.py27

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -2,65 +2,44 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 def encrypt(email="abc012"):
-    """
-    TODO: What is the objective? 
+    output = "" 
+    len_flag = len(email) != 6
+    anum_flag = email[:3] != 'abc' or email[3:] != '012' 
 
-    Args:
-        TODO: what arguments and data types are expected? (i.e., email)
-
-    Returns:
-        TODO: what varibale and data types are being returned?   
-    """
-    output = ""     
- len_flag = len(email) 
- anum_flag = email[:3] != 'abc' or email[3:] != '012'
-    if len_flag != 6:
-        print("Email must be 6 characters long.")        
-    elif anum_flag != 'abc' or email[3:] != '012':
-        print("Email must have 3 letters followed by 3 digits.")
-    else print(output) 
-    # TODO: fix line below, process our string into a list
-    email_lst = list(email)
-        
+    email_lst = ["a", "b", "c", "0", "1", "2"]
     # TODO: complete line(s) below, convert EACH new element into a string
     new_ascii = ord(email_lst[0]) + 3    
     email_lst[0] = chr(new_ascii)        
-        
+    if len(email_lst) != 6:
+        output += "Email must be 6 characters long."
+        logging.info(output)
+    if email_lst:
+        output += "Email must have 3 letters followed by 3 digits."
+    else:
+        logging.info(output)
+        print(output)     
+    
+      
     # TODO: fix line below, convert list into a string
-    email_str = "dbc012"
-
-    # keep all updates in the retVal (str) variablei
-    # i.e.,
-    #    email_str = " some string updates here "
-    #    email_1 = email_str.strip()
-    #    retVal = email_1
-    retVal = email_str
-    return retVal 
+email_str = "dbc012"
+   
+retVal = email_str
+print(retVal) 
 
 def decrypt(email="def345"):
-    
-    # input validation
     output = "" 
-    # TODO: fix line below and, implement functionality rather than literals
-    # keep all updates in the anum_flag (bool) variable
-    # i.e., 
-    #     A = email[:3] (check first half)
-    #     B = email[3:] (check second half)
-    #     enum_flag = A or B
- len_flag = len(email) 
- anum_flag = email[:3] != 'abc' or email[3:] != '012' 
-    if len_flag == len(email) != 6:
-        print("Email must be 6 characters long.")     
-    elif anum_flag == email[:3] != 'def' or email[3:] != '345':
-        print("Email must have 3 letters followed by 3 digits.") 
-    else print(output)
-
-    # TODO: apply the encrypt pseudocode but shift down 3
     
-    # keep all updates in the retVal (str) variablei
-    # i.e.,
-    #    email_str = " some string updates here "
-    #    email_1 = email_str.strip()
-    #    retVal = email_1
-    retVal = "aef345"
-    return retVal
+len_flag = len(email) != 6
+anum_flag = email[:3] != 'def' or email[3:] != '345' 
+if len(email_lst) != 6:
+        output += "Email must be 6 characters long."
+        logging.info(output)
+if email_lst:
+    output += "Email must have 3 letters followed by 3 digits."
+else:
+    logging.info(output)
+    print(output)
+
+   
+retVal = "aef345"
+print(retVal)
