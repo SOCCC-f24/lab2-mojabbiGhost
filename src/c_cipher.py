7d126487f9f1b9babb5f9ae5c0997244bd2a91cf
mojabbiGhost <ak132@sunyorange.edu>
2024-10-14 23:58:48 -0400

    Update c_cipher.py30

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -2,45 +2,87 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 def encrypt(email="abc012"):
-    output = "" 
-    len_flag = len(email) == 6
-    anum_flag = email[:3] != 'abc' or email[3:] != '012' 
+    """
+    TODO: What is the objective? 
 
+    Args:
+        TODO: what arguments and data types are expected? (i.e., email)
+
+    Returns:
+        TODO: what varibale and data types are being returned?   
+    """
+    output = "" 
+    len_flag = len(email) != 6
+    # TODO: fix line below and, implement functionality rather than literals
+    # keep all updates in the anum_flag (bool) variable
+    # i.e., 
+    #     A = email[:3] (check first half)
+    #     B = email[3:] (check second half)
+    #     enum_flag = A or B
+     anum_flag = email[:3] != 'def' or email[3:] != '345' 
+    if len_flag:                         # NOTE: here we provide input validation on length
+        a = email != 6
+        print("length check")
+    if anum_flag:                        # NOTE: here we provide input validation on alpha/nu
+        b = email[:3] != 'abc' or email[3:] !='012'
+        print("alpha/num check")
+        
+    # TODO: fix line below, process our string into a list
     email_lst = ["a", "b", "c", "0", "1", "2"]
-    email_str = string(email_lst)
+        
     # TODO: complete line(s) below, convert EACH new element into a string
-    new_ascii = ord(email_lst[0]) + 3    
-    email_lst[0] = chr(new_ascii)        
-    if len(email_lst) != 6:
-        output += "Email must be 6 characters long."
-        logging.info(output)
-    elif email_lst:
-        output += "Email must have 3 letters followed by 3 digits."
-    else:
-        logging.info(output)
-        print(output)     
-    
-      
+    new_ascii = ord(email_lst[0]) + 3    # NOTE: here we extract and update element at 0 
+    email_lst[0] = chr(new_ascii)        # NOTE: here we convert our ASCII into string
+        
     # TODO: fix line below, convert list into a string
-email_str = "dbc012"
-   
-retVal = email_str
-print(retVal) 
+    email_str = "dbc012"
+
+    # keep all updates in the retVal (str) variablei
+    # i.e.,
+    #    email_str = " some string updates here "
+    #    email_1 = email_str.strip()
+    #    retVal = email_1
+    retVal = email_str
+    return retVal 
 
 def decrypt(email="def345"):
+    """
+    TODO: What is the objective? 
+
+    Args:
+        TODO: what arguments and data types are expected? (i.e., email)
+
+    Returns:
+        TODO: what varibale and data types are being returned?   
+    """
+    # input validation
     output = "" 
-    
-len_flag = len(email) == 6
-anum_flag = email[:3] != 'def' or email[3:] != '345' 
-if len(email_lst) != 6:
+    len_flag = len(email) != 6
+    # TODO: fix line below and, implement functionality rather than literals
+    # keep all updates in the anum_flag (bool) variable
+    # i.e., 
+    #     A = email[:3] (check first half)
+    #     B = email[3:] (check second half)
+    #     enum_flag = A or B
+    anum_flag = email[:3] != 'def' or email[3:] != '345' 
+
+    if len_flag:                         # NOTE: here we provide input validation on length
+        output = "Length check failed\n"
         output += "Email must be 6 characters long."
         logging.info(output)
-elif email_lst:
-    output += "Email must have 3 letters followed by 3 digits."
-else:
-    logging.info(output)
-    print(output)
+        return output        
+    if anum_flag:                        # NOTE: here we provide input validation on alpha/num
+        output = "alpha num check failed\n"
+        output += "Email must have 3 letters followed by 3 digits."
+        logging.info(output)
+        return output   
 
-   
-retVal = "aef345"
-print(retVal)
+    # TODO: apply the encrypt pseudocode but shift down 3
+    
+    # keep all updates in the retVal (str) variablei
+    # i.e.,
+    #    email_str = " some string updates here "
+    #    email_1 = email_str.strip()
+    #    retVal = email_1
+    retVal = "aef345"
+    return retVal
